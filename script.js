efca0ac0ac9e843552bc13d64198367a72dbc2d8
Saikumar113 <102425358+Saikumar113@users.noreply.github.com>
2022-04-27 19:53:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+function add() {
+  var n1=parseInt(document.getElementById("num1").value);
+  var n2=parseInt(document.getElementById("num2").value);
+  var result = n1+n2;
+    alert(result);
+}
+function sub()
+{
+  var n1 =document.getElementById('num1').value;
+  var n2= document.getElementById('num2').value;
+  var result = n1-n2;
+    alert(result);
+}
+function mul() {
+  var n1 =document.getElementById('num1').value;
+  var n2= document.getElementById('num2').value;
+  var result = n1*n2;
+    alert(result);
+}
+function div() {
+  var n1 =document.getElementById('num1').value;
+  var n2= document.getElementById('num2').value;
+  var result = n1/n2;
+    alert(result);
+}
+function perc() {
+  var n1 =document.getElementById('num1').value;
+  var n2= document.getElementById('num2').value;
+  var result = n1%n2;
+    alert(result);
+
+}
